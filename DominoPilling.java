74474769a57be915a370382c172799d454571bbb
Sharuf <ali@gmail.com>
2024-10-28 04:09:31 +0530

    Problem no - 50/A

---
+++ DominoPilling.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DominoPilling {
+
+        public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int m = sc.nextInt();
+                int n = sc.nextInt();
+                System.out.println((int)(Math.floor((m*n)/2)));
+                sc.close();
+        }
+        
+}
